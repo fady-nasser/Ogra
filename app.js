78b45682f313fa3e5c5492761965ae1e6c1f9f0c
Fady Nasser <fadynasseraziz@gmail.com>
2025-06-07 14:50:23 +0300

    fixed few errors

--- app.js
+++ app.js
@@ -1,38 +1,38 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const choose = document.querySelector('.choose');
-    const chooseVehicle = document.querySelector('.choose-vehicle');
+// document.addEventListener("DOMContentLoaded", () => {
+//     const choose = document.querySelector('.choose');
+//     const chooseVehicle = document.querySelector('.choose-vehicle');
 
-    // Initial state: hidden and off-screen
-    choose.classList.remove('show');
+//     // Initial state: hidden and off-screen
+//     choose.classList.remove('show');
 
-    function showChoose() {
-        choose.classList.add('show');
-    }
+//     function showChoose() {
+//         choose.classList.add('show');
+//     }
 
-    function hideChoose() {
-        choose.classList.remove('show');
-        // Remove #choose from URL without reloading
-        if (window.location.hash === "#choose") {
-            history.pushState("", document.title, window.location.pathname + window.location.search);
-        }
-    }
+//     function hideChoose() {
+//         choose.classList.remove('show');
+//         // Remove #choose from URL without reloading
+//         if (window.location.hash === "#choose") {
+//             history.pushState("", document.title, window.location.pathname + window.location.search);
+//         }
+//     }
 
-    function handleHash() {
-        if (window.location.hash === "#choose") {
-            showChoose();
-        } else {
-            hideChoose();
-        }
-    }
+//     function handleHash() {
+//         if (window.location.hash === "#choose") {
+//             showChoose();
+//         } else {
+//             hideChoose();
+//         }
+//     }
 
-    window.addEventListener('hashchange', handleHash);
-    window.addEventListener('popstate', handleHash);
-    handleHash();
+//     window.addEventListener('hashchange', handleHash);
+//     window.addEventListener('popstate', handleHash);
+//     handleHash();
 
-    // Hide when clicking outside choose-vehicle
-    choose.addEventListener('mousedown', (e) => {
-        if (!chooseVehicle.contains(e.target)) {
-            hideChoose();
-        }
-    });
-});
+//     // Hide when clicking outside choose-vehicle
+//     choose.addEventListener('mousedown', (e) => {
+//         if (!chooseVehicle.contains(e.target)) {
+//             hideChoose();
+//         }
+//     });
+// });
