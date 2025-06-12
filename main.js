8ef1a64aaf523ee1cc97ddeeca02cda1ba8f586b
Fady Nasser <fadynasseraziz@gmail.com>
2025-06-12 07:48:04 +0300

    changing the theme color to always being the primary green color

--- main.js
+++ main.js
@@ -1,31 +1,13 @@
-function updateThemeColor(color) {
-  let metaThemeColor = document.querySelector("meta[name=theme-color]");
-  if (metaThemeColor) {
-    metaThemeColor.setAttribute("content", color);
-  } else {
-    metaThemeColor = document.createElement("meta");
-    metaThemeColor.name = "theme-color";
-    metaThemeColor.content = color;
-    document.head.appendChild(metaThemeColor);
-  }
-}
-
-// When switching theme:
-updateThemeColor("#ffffff"); // light mode
- // dark mode        
-        
-        
         //storage check for dark mode
         // document.addEventListener('DOMContentLoaded', () => {
             if (localStorage.getItem('darkMode') === 'enabled') {
                 document.querySelector("html").classList.add('dark-mode');
                 // document.querySelector("html").classList.add("no-transition");
                 // document.querySelector("html").classList.remove("no-transition");
-                updateThemeColor("#1d1d1d");
                 
             } else {
                 document.querySelector("html").classList.remove('dark-mode');
-                updateThemeColor("#ffeed3");
+                
             }
         //     // creating routes
         // if(window.location.pathname === '/Ogra/'){
